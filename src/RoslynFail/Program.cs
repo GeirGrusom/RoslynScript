2d9b27a997ccbb4ee443546567af5c588238aa4d
Henning Moe <hem@isys.no>
2016-10-24 08:21:56 +0200

    Switch to AddReference according to tmat. Still fails on my machine.

--- src/RoslynFail/Program.cs
+++ src/RoslynFail/Program.cs
@@ -9,15 +9,21 @@ namespace RoslynFail
 {
     public class Program
     {
+        public class Bar
+        {
+            public string Foo => "Hello World!";
+        }
+
         public static void Main(string[] args)
         {
             var options =
                 ScriptOptions.Default
-                .WithReferences("System.Runtime")
-                .WithReferences("System.Runtime, Version=4.1.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")
-                .WithReferences(typeof(object).GetTypeInfo().Assembly);
+                    //.AddReferences("System.Runtime")
+                    .AddReferences("System.Runtime, Version=4.1.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a");
+                //.AddReferences(typeof(object).GetTypeInfo().Assembly);
 
-            Microsoft.CodeAnalysis.CSharp.Scripting.CSharpScript.RunAsync("System.Console.WriteLine(Foo);", options, new { Foo ="Hello World!" } );
+            Microsoft.CodeAnalysis.CSharp.Scripting.CSharpScript.RunAsync("System.Console.WriteLine(Foo);", options, new Bar() );
         }
+        
     }
 }
